e1c3cb81e2c81b5fce6b649fbca905075cad51f7
haowenZheng <haowenz01@gmail.com>
2024-10-05 19:55:51 +0200

    processing data added

---
+++ src/data_processing/process_data.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+import string, nltk
+from nltk import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
